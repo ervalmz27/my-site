dd937e99731b1e31468973acb246d22d2f4cf0aa
ervalmz27 <naufalmtz27@gmail.com>
2023-04-21 18:26:02 +0700

    asset

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://github.com/ervalmz27/portpolio",
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000, // This is the port which we will use in docker
+  },
 });
